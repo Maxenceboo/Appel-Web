015a66af8e2297b4b1b64cd11cace2909cc00ee9
D3sTryk3 <maxenceboo@gmail.com>
2024-03-24 18:08:02 +0100

    Ajouter la fonctionnalité de recherche d'appels et de justification

--- src/view/Gestionnaire/Gestionnaire.tsx
+++ src/view/Gestionnaire/Gestionnaire.tsx
@@ -9,17 +9,51 @@ type Etudiant = {
 	prenom: string;
 }
 
+type Call = {
+    id_call: number;
+    date_heure: string;
+    abs$1: boolean;
+    code_etudiant: number;
+    id_user: number;
+    id_ue: number;
+    id_sousgrp: number;
+}
+
 const Gestionnaire: React.FC = () => {
 	const [etudiants, setEtudiants] = useState<Etudiant>();
 	const [nom, setNom] = useState<string>('');
 	const [prenom, setPrenom] = useState<string>('');
 	const [error, setError] = useState<string>('');
+	const [justification, setJustification] = useState<string>('');
+	const [calls, setCalls] = useState<Call[]>();
 
 
 	const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		try {
 			let response = await axios.get(`/etudiant/${nom}/${prenom}`);
+			setEtudiants(response.data);
+		} catch (error) {
+			console.log(error);
+			setError('Etudiant non trouvé');
+		}
+
+		// get all calls
+		try {
+			if (etudiants) {
+				let response = await axios.get(`/un_call/${etudiants.code_etudiant}`);
+				setCalls(response.data);
+			}
+		} catch (error) {
+			console.log(error);
+			setError('Calls non trouvés');
+		}
+	}
+
+	const handleSend = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		try {
+			let response = await axios.post(`/etudiant/${nom}/${prenom}`);
 			setEtudiants(response.data);
 		} catch (error) {
 			console.log(error);
@@ -64,14 +98,39 @@ const Gestionnaire: React.FC = () => {
 								<h3>Nom: {etudiants.nom}</h3>
 								<h3>Prenom: {etudiants.prenom}</h3>
 							</div>
-							{/* add inuput text for justify  */}
-							<div className="justify">
-								<h3>Justification</h3>
-								<input type="text" className="gestionnaire-input"/>
-							</div>
 						</div>
 					)
 				}
+				{
+					// print all call de l'etudiant with checkbox 
+					calls && (
+						<div className="gestionnaire-etudiant">
+							<h2>Appels</h2>
+							{
+								calls.map((call) => (
+									<div key={call.id_call}>
+										<h3>{call.date_heure}</h3>
+										<h3>{call.abs$1}</h3>
+										<input type="checkbox" />
+									</div>
+								))
+							}
+						</div>
+					)
+
+				}
+				{
+					etudiants && (
+						<div className="justify">
+							<form onSubmit={handleSend} className="gestionnaire-form">
+								<h3>Justification</h3>
+								<input type="text" className="gestionnaire-input" onChange={(e) => {setJustification(e.target.value)}}/>
+								<button type="submit" className="gestionnaire-button">Envoyer</button>
+							</form>
+						</div>
+					)
+				}
+
 				{
 					/* print error message */
 					error && (
@@ -79,24 +138,7 @@ const Gestionnaire: React.FC = () => {
 							<h3>{error}</h3>
 						</div>
 					)
-				}
-				<div className="gestionnaire-etudiant">
-							<h2>Etudiant</h2>
-							<div>
-								<h3>Code etudiant: 54364536453</h3>
-								<h3>Nom: dfghdfghdfgh</h3>
-								<h3>Prenom: dfghfdgh</h3>
-							</div>
-							{/* add inuput text for justify  */}
-							<div className="justify">
-								<h3>Justification</h3>
-								<input type="text" className="gestionnaire-input"/>
-							</div>
-						</div>
-				<div className="gestionnaire-error">
-					<h3>error</h3>
-				</div>
-				
+				}				
 			</div>
 		</div>
 	);
